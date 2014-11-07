64d2f537f11c9480f997083371663e4ed1c3ab7b
David Lemayian <dlemayian@gmail.com>
2014-11-07 19:58:37 +0300

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 DC::Application.initialize!
-
-# CUSTOM: Edit AWS default host
-AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
