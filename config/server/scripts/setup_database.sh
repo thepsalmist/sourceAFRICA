233beb5770c88e9703d8c2164c93b647098177c8
David Lemayian <dlemayian@gmail.com>
2014-12-30 18:01:18 +0300

    Update setup_database.sh

--- config/server/scripts/setup_database.sh
+++ config/server/scripts/setup_database.sh
@@ -3,7 +3,7 @@
 USERNAME=ubuntu
 RAILS_ENVIRONMENT=production
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ utopic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
