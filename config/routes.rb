494d20b4c2ebc2b51467f1ab3bf99e7f361456a2
David Lemayian <dlemayian@gmail.com>
2015-01-20 22:52:56 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -154,7 +154,7 @@ DC::Application.routes.draw do
   get '/p3p.:format' => 'home#p3p', :as => :p3p
   get '/home' => 'home#index', :as => :home
   get '/news' => 'home#news', :as => :news
-  get '/opensource' => 'home#opensource', :as => :opensource
+  get '/opensource' => redirect("https://github.com/CodeForAfrica/sourceAFRICA"), :as => :opensource
   get '/about' => 'home#about', :as => :about
   get '/contact' => 'home#contact', :as => :contact
   get '/help' => 'home#help'
